1a8cc953ccdaca1ebeb774c95ba06e75349f6e0e
Ibrohimbek <programming0127@gmail.com>
2024-12-27 23:23:09 +0900

    feat: 2024-12-27 - 11: 23

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,7 @@
 # !/bin/bash
-
 # PRODUCTION
 git reset --hard
 git checkout master
 git pull origin master
 
-npm install --legacy-peer-deps
-npm install --legacy-peer-deps serve -g
-npm run build
-pm2 start process.config.js --env production
+docker compose up -d
--- src/app/context/SocketContext.tsx
+++ src/app/context/SocketContext.tsx
@@ -1,8 +1,9 @@
 import React, { createContext } from "react";
 import { io, Socket } from "socket.io-client";
+import { serverApi } from "../../lib/config";
 
-const socket = io(process.env.REACT_APP_API_URL as string, {
-  withCredentials: true,
+const socket = io(serverApi, {
+	withCredentials: true,
 });
 export const SocketContext = createContext<Socket>(socket);
 
