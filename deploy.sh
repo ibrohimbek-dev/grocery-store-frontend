4661ec643cb927500367ec0c3c3844eb046d4637
Ibrohimbek <programming0127@gmail.com>
2024-12-27 21:31:07 +0900

    feat: 2024-12-27 - 21: 30

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git reset --hard
 git checkout master
 git pull origin master
 
-npm install
-npm install serve -g
+npm install --legacy-peer-deps
+npm install --legacy-peer-deps serve -g
 npm run build
 pm2 start process.config.js --env production
